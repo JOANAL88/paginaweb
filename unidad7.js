6de8dead14b4b7113b63cf889ed7e901f88dfffb
JOANAL88 <126472125+JOANAL88@users.noreply.github.com>
2024-01-09 20:49:34 -0300

    Add files via upload

---
+++ unidad7.js
@@ -0,0 +1,9 @@
+var nombre = prompt("¿Cual es tu nombre?")
+alert (nombre + "  Bienvenido a nuestro sitio web")
+var publicidad = prompt("¿Que te trajo a nuestro sitio?")
+var intereses = prompt("¿Sobre qué productos te interesaría consultar?")
+var cantidadvista = prompt("¿Cuántos de nuestros dibujos has visto?")
+var unidadestotales = 378
+var unidadesrestantes = unidadestotales-cantidadvista
+var seleccion = prompt("¿Qué tipo de dibujo llama más tu atención?")
+var lugar = prompt("¿En qué lugar te gustaría plasmarlo?")
---
+++ unidad8.js
@@ -0,0 +1,11 @@
+function mostrarCaja(){
+    let fototecnica = document.getElementById("texto1")
+    fototecnica.style.display = "block"
+}
+if(cantidadvista==unidadestotales) {
+    let caja8 = document.getElementById("texto3")
+    caja8.style.display = "block"
+} else{
+    let caja8 = document.getElementById("texto3a")
+    caja8.style.display = "block"
+}
